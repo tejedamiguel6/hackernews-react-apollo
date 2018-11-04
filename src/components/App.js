32ee064d27ef63d9b8f2867cf3f8d5ae6e1b7f67
MiguelTejeda <tejedamiguel6@aol.com>
2018-11-04 13:37:35 -0500

    first commit, learning how to graphql

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="orange">
+          <p>HELLO</p>
+      </div>
+    );
+  }
+}
+
+export default App;
